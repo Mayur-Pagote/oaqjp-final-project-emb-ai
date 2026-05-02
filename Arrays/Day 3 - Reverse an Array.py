52dd3981554eb14b50101d96b5f47924a5a3884e
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-05-02 07:56:03 +0530

    Files deleted

--- Arrays/Day 3 - Reverse an Array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    # Define the method reverseArray that takes a list 'arr' as input
-    def reverseArray(self, arr):
-        # Reverse the array in place using the reverse() method
-        # This modifies the original list directly
-        arr.reverse()
-        
-        # Return the reversed array (which has been modified in place)
-        return arr
